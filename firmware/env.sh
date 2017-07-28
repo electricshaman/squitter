dac16d10b463a31fbe0376450ec7c8e6b0e264d7
Jeff Smith <jeff@sohlsmith.com>
2017-07-28 00:44:02 -0500

    Update env.sh

--- firmware/env.sh
+++ firmware/env.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Helper file that can be sourced to export common env needed for testing and deploying.
+
+# This file shouldn't be commited to your repo.
+# If it shows up in your git status, run the following:
+# git update-index --assume-unchanged env.sh
+
 export MIX_TARGET=rpi3
-export SQUITTER_NETWORK_SSID='ssid'
-export SQUITTER_NETWORK_PSK='password'
-export SQUITTER_NETWORK_KEY_MGMT='WPA-PSK'
+export SQUITTER_NETWORK_SSID="White Government Van"
+export SQUITTER_NETWORK_PSK="nothingtoseehere"
+export SQUITTER_NETWORK_KEY_MGMT="WPA-PSK"
