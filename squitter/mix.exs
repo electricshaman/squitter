af60cd9466e879412d04e631ba589c5567bbb5f4
Jeff Smith <jeff@sohlsmith.com>
2017-07-27 22:06:22 -0500

    Add make clean callout from elixir_make

--- squitter/mix.exs
+++ squitter/mix.exs
@@ -12,6 +12,7 @@ defmodule Squitter.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      compilers: [:elixir_make] ++ Mix.compilers,
+     make_clean: ["clean"],
      deps: deps()]
   end
 
