92df483ffe2e3822afb1e0471a34e1efe83c3405
Jeff Smith <jeff@sohlsmith.com>
2017-07-30 17:17:18 -0500

    Add GenStage to deps

--- web/mix.exs
+++ web/mix.exs
@@ -36,7 +36,8 @@ defmodule Squitter.Web.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:gen_stage, "~> 0.11"}
     ]
   end
 
