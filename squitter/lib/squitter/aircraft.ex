1da5ff869163909d540692bd34f68e9a53ae9f9a
Jeff Smith <jeff@sohlsmith.com>
2017-08-13 16:14:40 -0500

    Move great circle calculations to utils/math module

--- squitter/lib/squitter/aircraft.ex
+++ squitter/lib/squitter/aircraft.ex
@@ -299,12 +299,12 @@ defmodule Squitter.Aircraft do
       if even.index > odd.index do
         {_, lat} = lat_even
         {_, lon} = calculate_lon(lat_even, lon_pair)
-        distance = calculate_distance({lat, lon}, state.site_location)
+        distance = calculate_gcd({lat, lon}, state.site_location)
         {:ok, %{state | lat: lat, lon: lon, distance: distance}}
       else
         {_, lat} = lat_odd
         {_, lon} = calculate_lon(lat_odd, lon_pair)
-        distance = calculate_distance({lat, lon}, state.site_location)
+        distance = calculate_gcd({lat, lon}, state.site_location)
         {:ok, %{state | lat: lat, lon: lon, distance: distance}}
       end
     else
@@ -313,37 +313,6 @@ defmodule Squitter.Aircraft do
       {:ok, state}
     end
   end
-
-  defp calculate_distance(coord1, coord2, unit \\ :NM)
-  defp calculate_distance({_lat0, _lon0}, :unknown, _unit),
-    do: 0.0
-  defp calculate_distance({lat0, lon0}, {lat1, lon1}, unit) do
-    lat0 = lat0 * pi() / 180.0
-    lon0 = lon0 * pi() / 180.0
-    lat1 = lat1 * pi() / 180.0
-    lon1 = lon1 * pi() / 180.0
-
-    dlat = abs(lat1 - lat0)
-    dlon = abs(lon1 - lon0)
-
-    result_m =
-      if dlat < 0.001 && dlon < 0.001 do
-        a = sin(dlat / 2) * sin(dlat / 2) + cos(lat0) * cos(lat1) * sin(dlon / 2) * sin(dlon / 2)
-        6.371e6 * 2 * atan2(sqrt(a), sqrt(1.0 - a))
-      else
-        6.371e6 * acos(sin(lat0) * sin(lat1) + cos(lat0) * cos(lat1) * cos(dlon))
-      end
-
-    convert(:m, unit, result_m)
-    |> Float.round(1)
-  end
-
-  defp convert(:m, :NM, x),
-    do: x / 1852.0
-  defp convert(:m, :m, x),
-    do: x
-  defp convert(_, _, _x),
-    do: {:error, :unknown_unit}
 
   def in_sane_lat_zone?({_, lat1}, {_, lat2}) do
     in_same_lat_zone?(lat1, lat2)
--- squitter/lib/squitter/utils/math.ex
+++ squitter/lib/squitter/utils/math.ex
@@ -56,4 +56,38 @@ defmodule Squitter.Utils.Math do
     b = acos(1 - c/d)
     floor(a/b)
   end
+
+  @doc """
+  Calculate great circle distance between `coord1` and `coord2`
+  """
+  def calculate_gcd(coord1, coord2, unit \\ :NM)
+  def calculate_gcd({_lat0, _lon0}, :unknown, _unit),
+    do: 0.0
+  def calculate_gcd({lat0, lon0}, {lat1, lon1}, unit) do
+    lat0 = lat0 * pi() / 180.0
+    lon0 = lon0 * pi() / 180.0
+    lat1 = lat1 * pi() / 180.0
+    lon1 = lon1 * pi() / 180.0
+
+    dlat = abs(lat1 - lat0)
+    dlon = abs(lon1 - lon0)
+
+    result_m =
+      if dlat < 0.001 && dlon < 0.001 do
+        a = sin(dlat / 2) * sin(dlat / 2) + cos(lat0) * cos(lat1) * sin(dlon / 2) * sin(dlon / 2)
+        6.371e6 * 2 * atan2(sqrt(a), sqrt(1.0 - a))
+      else
+        6.371e6 * acos(sin(lat0) * sin(lat1) + cos(lat0) * cos(lat1) * cos(dlon))
+      end
+
+    convert(:m, unit, result_m)
+    |> Float.round(1)
+  end
+
+  defp convert(:m, :NM, x),
+    do: x / 1852.0
+  defp convert(:m, :m, x),
+    do: x
+  defp convert(_, _, _x),
+    do: {:error, :unknown_unit}
 end
