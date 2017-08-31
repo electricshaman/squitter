54d3f3dd4dd821473da87f3fd78722eaa6767ccb
Jeff Smith <jeff@sohlsmith.com>
2017-08-31 00:37:32 -0500

    More robust aircraft process monitoring by the state report process

--- squitter/lib/squitter/state_report.ex
+++ squitter/lib/squitter/state_report.ex
@@ -3,6 +3,7 @@ defmodule Squitter.StateReport do
   Process which holds the current valid state for all active aircraft.
   """
   use GenServer
+  require Logger
 
   @table :state_report
 
@@ -10,21 +11,25 @@ defmodule Squitter.StateReport do
     GenServer.start_link(__MODULE__, @table, name: __MODULE__)
   end
 
-  def init(table) do
-    #Registry.register(Squitter.ReportRegistry, "state", [])
-    table = :ets.new(table, [:named_table, {:write_concurrency, true}])
-    {:ok, {table, %{}}}
-  end
+  # Client
 
-  def state_changed(address, state) do
-    GenServer.cast(__MODULE__, {:state_changed, address, state})
-    #Registry.dispatch(Squitter.ReportRegistry, "state", fn entries ->
-    #  for {pid, _} <- entries, do: GenServer.cast(pid, {:state_changed, ac_address, ac_state})
-    #end)
+  def state_changed(address, ac_state) do
+    GenServer.cast(__MODULE__, {:state_changed, address, ac_state})
   end
 
   def age_changed(address, age) do
     GenServer.cast(__MODULE__, {:age_changed, address, age})
+  end
+
+  # Server
+
+  def init(table) do
+    _ = :ets.new(table, [:named_table, {:write_concurrency, true}])
+
+    # TODO: Implement Bimap data structure (like from boost) for tracking aircraft processes
+    # bidirectionally from address -> ref and ref -> address.
+
+    {:ok, {table, {Map.new, Map.new}}}
   end
 
   def handle_call(:state_report, _from, {table, aircraft}) do
@@ -35,8 +40,8 @@ defmodule Squitter.StateReport do
     {:reply, %{aircraft: report}, {table, aircraft}}
   end
 
-  def handle_cast({:state_changed, address, state}, {table, aircraft}) do
-    :ets.insert(table, {address, state})
+  def handle_cast({:state_changed, address, ac_state}, {table, aircraft}) do
+    :ets.insert(table, {address, ac_state})
     {:noreply, {table, aircraft}}
   end
 
@@ -44,26 +49,48 @@ defmodule Squitter.StateReport do
     case :ets.lookup(table, address) do
       [] ->
         {:noreply, {table, aircraft}}
-      [{^address, state}] ->
-        new_state = %{state | age: age}
-        :ets.insert(table, {address, new_state})
+      [{^address, ac_state}] ->
+        :ets.insert(table, {address, Map.replace(ac_state, :age, age)})
         {:noreply, {table, aircraft}}
     end
   end
 
-  def handle_info({:register, Squitter.AircraftRegistry, address, pid, _}, {table, aircraft}) do
+  def handle_info({:register, Squitter.AircraftRegistry, address, pid, _}, {table, {left, right}}) do
     ref = Process.monitor(pid)
-    {:noreply, {table, Map.put(aircraft, ref, address)}}
+    new_aircraft = {Map.put(left, address, ref), Map.put(right, ref, address)}
+    {:noreply, {table, new_aircraft}}
   end
 
-  def handle_info({:DOWN, ref, :process, _pid, _reason}, {table, aircraft}) do
+  def handle_info({:unregister, Squitter.AircraftRegistry, address, _pid}, {table, {left, right}}) do
+    # Lookup aircraft from the left (address -> ref)
+    new_aircraft =
+      case Map.pop(left, address) do
+        {nil, ^left} -> left
+        {ref, new_left} when is_reference(ref) ->
+          Logger.debug("Cleaning up report for #{address}")
+          true = cleanup(table, address, ref)
+          new_right = Map.delete(right, ref)
+          {new_left, new_right}
+      end
+    {:noreply, {table, new_aircraft}}
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {table, {left, right}}) do
+    # Lookup aircraft from the right (ref -> address)
+    new_aircraft =
+      case Map.pop(right, ref) do
+        {nil, ^right} -> right
+        {address, new_right} when is_binary(address) ->
+          Logger.debug("Cleaning up report for #{address}")
+          true = cleanup(table, address, ref)
+          new_left = Map.delete(left, address)
+          {new_left, new_right}
+      end
+    {:noreply, {table, new_aircraft}}
+  end
+
+  defp cleanup(table, address, ref) do
     Process.demonitor(ref)
-    case Map.pop(aircraft, ref) do
-      {nil, _} ->
-        {:noreply, {table, aircraft}}
-      {address, new_aircraft} ->
-        true = :ets.delete(table, address)
-        {:noreply, {table, new_aircraft}}
-    end
+    :ets.delete(table, address)
   end
 end
