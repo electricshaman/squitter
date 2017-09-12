b275acadc0db2c2a249d779499cf7cccb6b65c75
Jeff Smith <jeff@letote.com>
2017-09-11 22:12:49 -0700

    Rename avr_stage.ex to avr_tcp_stage.ex

--- squitter/lib/squitter/avr_tcp_stage.ex
+++ squitter/lib/squitter/avr_tcp_stage.ex
@@ -16,12 +16,13 @@ defmodule Squitter.AvrTcpStage do
     send(self(), :connect)
 
     hash_function = fn({_t,f} = m) ->
-      address = case Squitter.Decoding.ModeS.icao_address(f) do
-        {:ok, address} -> address
-        :error ->
-          Logger.warn("Failed to parse address needed for assigning partition: #{inspect f}")
-          ""
-      end
+      address =
+        case Squitter.Decoding.ModeS.icao_address(f) do
+          {:ok, address} -> address
+          :error ->
+            Logger.warn("Failed to parse address needed for assigning partition: #{inspect f}")
+            ""
+        end
       partition = :erlang.phash2(address, length(partitions))
       {m, partition}
     end
