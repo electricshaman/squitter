7a0dfbc125118b764d192f02b42b36596f6d4ac6
Jeff Smith <electricshaman@users.noreply.github.com>
2021-10-01 15:43:52 -0500

    Fix static asset issue

--- web/assets/brunch-config.js
+++ web/assets/brunch-config.js
@@ -36,7 +36,7 @@ exports.config = {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(assets\/static)/,
+    assets: [/^(static\/images)/, /^(static\/fonts)/],
   },
 
   // Phoenix paths configuration
